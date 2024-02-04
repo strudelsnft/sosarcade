0b1db2e3d6165b67973ab0d2e9790f360a4498a0
doctorsolana <hashinja@protonmail.com>
2024-02-04 20:26:09 +0100

    fixed token settings

--- src/constants.ts
+++ src/constants.ts
@@ -13,13 +13,13 @@ export const PLATFORM_SHARABLE_URL = 'v2-play.gamba.so'
 // List of tokens supported by this platform
 export const TOKENS: TokenMeta[] = [
   GambaStandardTokens.sol,
+  {
+    mint: new PublicKey("2mnGSkXH1h6x5qmhwoQzAZDKa83vnRf8wNkNWVbdv7w5"),
+    symbol: '$SOS',
+    name: 'Strudels on Solana',
+    image: "https://img.fotofolio.xyz/?url=https%3A%2F%2Farweave.net%2FQbMVjKRsPJp2mghhed--03EhCeSS68Vum6QlxPgCRvo",
+    decimals: 2,
+    baseWager: 20000 * 1e2,
+  },
   GambaStandardTokens.usdc,
-  // {
-  //   mint: new PublicKey("2mnGSkXH1h6x5qmhwoQzAZDKa83vnRf8wNkNWVbdv7w5"),
-  //   symbol: '$SOS',
-  //   name: 'Strudels on Solana',
-  //   image: "https://img.fotofolio.xyz/?url=https%3A%2F%2Farweave.net%2FQbMVjKRsPJp2mghhed--03EhCeSS68Vum6QlxPgCRvo",
-  //   decimals: 1e9,
-  //   baseWager: 1,
-  // }
 ]
